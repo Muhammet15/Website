aa418313149700ba3be20d12e6deb782742f841e
Muhammet Ali Kayacan <58929064+Muhammet15@users.noreply.github.com>
2022-12-28 15:55:56 +0300

    navbar düzenlendi post detail düzenlendi

---
+++ app.js
@@ -0,0 +1,14 @@
+const menukapali = document.querySelector(".menu__kapali");
+const linkscatch = document.querySelector(".app__links");
+    if (menukapali && linkscatch ) {
+        menukapali.addEventListener('click',()=>{ 
+            linkscatch.classList.toggle("active");
+       
+            if (linkscatch.classList.contains("active")) {
+                menukapali.src ="./assets/images/acik.svg";
+            } 
+            else{
+                menukapali.src ="./assets/images/kapalı.svg";
+            }
+});
+    }
