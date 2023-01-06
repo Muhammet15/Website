29026f20f868649c9c69fff9a37773d89ed5433a
Muhammet Ali Kayacan <58929064+Muhammet15@users.noreply.github.com>
2023-01-06 23:05:44 +0300

    yeni eklemeler yapıldı Mail yapıldı

--- contact.js
+++ contact.js
@@ -1,20 +1,65 @@
 var btn = document.getElementById('btnsend');
+var btncancel = document.getElementById('btncancel');
+btncancel.addEventListener('click',function(e){
+  location.reload();
+});
 btn.addEventListener('click',function(e){
         e.preventDefault()
         var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
-        var subject = document.getElementById('subject').ariaValueMax;
-        var message = document.getElementById('message').ariaValueMax;
-        var body ='name: '+name+ '<br/> email: '+email+'<br/> subject: '+subject+'<br/> message: '+message;
-        Email.send({
-            Host : "smtp.gmail.com",
+        var email = document.getElementById('email_id').value;
+        var tel = document.getElementById('no').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+        var body =
+        '<b>name:</b> '+name+ 
+        '<br/>  <b>email: </b>'+email+
+        '<br/> <b>subject: </b>'+subject+
+        '<br/> <b>tel:</b>'+tel+
+        '<br/> <b>message: </b>'+message;
+        const validateEmail = (email) => {
+          return String(email)
+            .toLowerCase()
+            .match(
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+        };
+        
+        // console.log(name);
+        // console.log(email);
+        // console.log(subject);
+        // console.log(message);
+        // console.log(tel);
+        // console.log(body);
+      //   Email.send({
+      //     SecureToken : "a276723c-85c8-44a3-9cb8-ff1341e98689",
+      //     To : 'them@website.com',
+      //     From : "you@isp.com",
+      //     Subject : "This is the subject",
+      //     Body : "And this is the body"
+      // }).then(
+      //   message => alert(message)
+      // );
+      if(email && name && message )
+      {
+        if(validateEmail(email)){
+          Email.send({
+            Host : "smtp.elasticemail.com",
             Username : "muhammetluna@gmail.com",
-            Password : "dwemtyjwqolbdcbz",
-            To : 'muhammetluna@gmail.com"',
-            From : email,
+            Password : "7E5738DE2B315131C97299A7A2B2169B5239",
+            To :  'muhammetluna@gmail.com',
+            From : 'muhammetluna@gmail.com',
             Subject : subject,
             Body : body
         }).then(
-          message => alert(message)
+          message => alert("Mailiniz gönderilmiştir..")
         );
+        }
+        else{
+          alert("Mail Yanlış..");
+        }
+      }
+      else{
+        alert("Eksik Bilgiler var..");
+      }
+       
 });
