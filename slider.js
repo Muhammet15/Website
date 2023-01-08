cb83cb9fb908b12b86d3a8f773ab4551f18b784e
Muhammet Ali Kayacan <58929064+Muhammet15@users.noreply.github.com>
2023-01-08 16:08:12 +0300

    sayfalar düzenlendi yenilikler yapıldı.

---
+++ slider.js
@@ -0,0 +1,47 @@
+const items = document.querySelectorAll('.img');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+  document.getElementById('output').innerHTML = count;
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+  document.getElementById('output').innerHTML = count;
+}
+function keyPress(e) {
+  e = e || window.event;
+  
+  if (e.keyCode == '37') {
+    showPreviousItem();
+    
+  } else if (e.keyCode == '39') {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+document.addEventListener('keydown', keyPress);
