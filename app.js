b38fff29e18a7194fa34bfd21cfc87428e0791a2
Muhammet Ali Kayacan <58929064+Muhammet15@users.noreply.github.com>
2023-01-10 16:02:14 +0300

    Translate eng

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const linkscatch = document.querySelector(".app__links");
     if (menukapali && linkscatch ) {
         menukapali.addEventListener('click',()=>{ 
             linkscatch.classList.toggle("active");
-       
             if (linkscatch.classList.contains("active")) {
-                menukapali.src ="./assets/images/acik.svg";
+                menukapali.src ="../assets/images/acik.svg";
             } 
             else{
-                menukapali.src ="./assets/images/kapalı.svg";
+                menukapali.src ="../assets/images/kapalı.svg";
             }
 });
     }
